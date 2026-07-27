f82b8340ca36b655d5476a2a56964cce67aeef6b
Ahteshamul Hasan <ahteshamulhasan2@gmail.com>
2026-07-27 18:42:06 +0600

    feat: add environment configuration file with API and Zego integration constants

--- store/config/envConfig.ts
+++ store/config/envConfig.ts
@@ -24,8 +24,6 @@ export const url = `${imgUrl}api/v1/`;
 //   password: `${process.env.NEXT_PUBLIC_GUEST_USER_PASSWORD}`
 // };
 
-console.log(imgUrl, "asdf");
-
 // 🔥 WebSocket base URL (auto picks wss:// if https page, ws:// otherwise)
 // const wsProtocol = window.location.protocol === "https:" ? "wss" : "ws";
 // export const wsUrl = `${wsProtocol}://${new URL(imgUrl).host}`;
